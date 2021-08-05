9076f4e9bd7576e19343ec52fdd6305b208b6a28
Justin Turner <jmturner638@gmail.com>
2021-08-05 07:31:48 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,24 @@
+from television_program import TelevisionProgram
+
 def main():
     #write your code below this line
+    shows = []
+
+    while True:
+        name = input("Name:")
+
+        if not name:
+            break
+
+        duration = int(input("Duration:"))
+
+        shows.append(TelevisionProgram(name, duration))
+
+    max_duration = int(input("Program's maximum duration?"))
+
+    for show in shows:
+        if show.get_duration() <= max_duration:
+            print(show)
 
 if __name__ == '__main__':
     main()
